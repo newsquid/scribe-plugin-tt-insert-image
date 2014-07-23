14ff39a6310a74d063209c4866e00b676d266054
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2014-07-23 16:01:44 +0200

    Built project. Stupid thing to miss.

--- scribe-plugin-tt-insert-image.js
+++ scribe-plugin-tt-insert-image.js
@@ -1,11 +1,18 @@
-define(function(){
+define('scribe-plugin-tt-insert-image', function(){
 
   'use strict';
 
   return function(){
-
     return function(scribe){
 
+        var TTInsertImageCommand = new scribe.api.Command("tt_insertImage");
+
+        TTInsertImageCommand.nodeName = 'img';
+
+        TTInsertImageCommand.execute = function() {
+            alert("Here's a story, all about how");
+            alert("my life got flipped, turned upside down");
+        };
 
     };
   };
